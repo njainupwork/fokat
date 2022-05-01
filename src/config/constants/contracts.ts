efaff771d125fda24ab075adc23c4bb816fbec41
Eric Muigai <emuigai5@gmail.com>
2022-05-01 11:03:40 +0300

    updates

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,7 +1,7 @@
 export default {
   board: {
     97: "0x094714b939981d92620e6cff1f7d4147c754bcd4",
-    56: "0xb4B5015c389fd2D6A6a76E2e005b7C4D8b97D73f",
+    56: "0xccace04f3239107f9a264913828ff3badc65d1ba",
   },
   nft: {
     97: "0xf762ad2378e049cf7b5c42a212210a69dac4f624",
