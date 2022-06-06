8ce019812a1da399f23b88354e1eb3d9699dbb11
Nikhil Jain <nikhiljainupwork@gmail.com>
2022-06-06 10:42:33 +0530

    Updated game

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,10 @@
-import "./app.css";
-import { Fragment, useEffect, useState } from "react";
-import Unity, { UnityContext } from "react-unity-webgl";
+import "./app.css"
+import { Fragment, useEffect, useState } from "react"
+import Unity, { UnityContext } from "react-unity-webgl"
 
 interface Vector2 {
-  x: number;
-  y: number;
+  x: number
+  y: number
 }
 
 // This is the context that Unity will use to communicate with the React app.
@@ -13,97 +13,97 @@ const unityContext = new UnityContext({
   companyName: "Jeffrey Lanters",
   // The url's of the Unity WebGL runtime, these paths are public and should be
   // accessible from the internet and relative to the index.html.
-  loaderUrl: "unitybuild/2020.1/myunityapp.loader.js",
-  dataUrl: "unitybuild/2020.1/myunityapp.data",
-  frameworkUrl: "unitybuild/2020.1/myunityapp.framework.js",
-  codeUrl: "unitybuild/2020.1/myunityapp.wasm",
+  loaderUrl: "unitybuild/2020.1/WebGL.loader.js",
+  dataUrl: "unitybuild/2020.1/WebGL.data",
+  frameworkUrl: "unitybuild/2020.1/WebGL.framework.js",
+  codeUrl: "unitybuild/2020.1/WebGL.wasm",
   streamingAssetsUrl: "unitybuild/2020.1/streamingassets",
   // Additional configuration options.
   webglContextAttributes: {
     preserveDrawingBuffer: true,
   },
-});
+})
 
 // This is the React component that will be rendering the Unity app.
 function App() {
   // The app's state.
-  const [isUnityMounted, setIsUnityMounted] = useState<boolean>(true);
-  const [rotationSpeed, setRotationSpeed] = useState<number>(30);
-  const [cubeRotation, setCubeRotation] = useState<number>(0);
-  const [clickPosition, setClickPosition] = useState<Vector2>({ x: 0, y: 0 });
-  const [saidMessage, setSaidMessage] = useState<string>("Nothing");
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
-  const [progression, setProgression] = useState<number>(0);
+  const [isUnityMounted, setIsUnityMounted] = useState<boolean>(true)
+  const [rotationSpeed, setRotationSpeed] = useState<number>(30)
+  const [cubeRotation, setCubeRotation] = useState<number>(0)
+  const [clickPosition, setClickPosition] = useState<Vector2>({ x: 0, y: 0 })
+  const [saidMessage, setSaidMessage] = useState<string>("Nothing")
+  const [isLoaded, setIsLoaded] = useState<boolean>(false)
+  const [progression, setProgression] = useState<number>(0)
 
   // When the component is mounted, we'll register some event listener.
   useEffect(() => {
-    unityContext.on("canvas", handleOnUnityCanvas);
-    unityContext.on("progress", handleOnUnityProgress);
-    unityContext.on("loaded", handleOnUnityLoaded);
-    unityContext.on("RotationDidUpdate", handleOnUnityRotationDidUpdate);
-    unityContext.on("ClickedPosition", handleOnUnityClickedPosition);
-    unityContext.on("Say", handleOnUnitySayMessage);
+    unityContext.on("canvas", handleOnUnityCanvas)
+    unityContext.on("progress", handleOnUnityProgress)
+    unityContext.on("loaded", handleOnUnityLoaded)
+    unityContext.on("RotationDidUpdate", handleOnUnityRotationDidUpdate)
+    unityContext.on("ClickedPosition", handleOnUnityClickedPosition)
+    unityContext.on("Say", handleOnUnitySayMessage)
     // When the component is unmounted, we'll unregister the event listener.
     return function () {
-      unityContext.removeAllEventListeners();
-    };
-  }, []);
+      unityContext.removeAllEventListeners()
+    }
+  }, [])
 
   // When the rotation speed has been updated, it will be sent to Unity.
   useEffect(() => {
-    unityContext.send("MeshCrate", "SetRotationSpeed", rotationSpeed);
-  }, [rotationSpeed]);
+    unityContext.send("MeshCrate", "SetRotationSpeed", rotationSpeed)
+  }, [rotationSpeed])
 
   // Built-in event invoked when the Unity canvas is ready to be interacted with.
   function handleOnUnityCanvas(canvas: HTMLCanvasElement) {
-    canvas.setAttribute("role", "unityCanvas");
+    canvas.setAttribute("role", "unityCanvas")
   }
 
   // Built-in event invoked when the Unity app's progress has changed.
   function handleOnUnityProgress(progression: number) {
-    setProgression(progression);
+    setProgression(progression)
   }
 
   // Built-in event invoked when the Unity app is loaded.
   function handleOnUnityLoaded() {
-    setIsLoaded(true);
+    setIsLoaded(true)
   }
 
   // Custom event invoked when the Unity app sends a message indicating that the
   // rotation has changed.
   function handleOnUnityRotationDidUpdate(degrees: number) {
-    setCubeRotation(Math.round(degrees));
+    setCubeRotation(Math.round(degrees))
   }
 
   // Custom event invoked when the Unity app sends a message indicating that the
   // mouse click position has changed.
   function handleOnUnityClickedPosition(x: number, y: number) {
-    setClickPosition({ x, y });
+    setClickPosition({ x, y })
   }
 
   // Custom event invoked when the Unity app sends a message including something
   // it said.
   function handleOnUnitySayMessage(message: string) {
-    setSaidMessage(message);
+    setSaidMessage(message)
   }
 
   // Event invoked when the user clicks the button, the speed will be increased.
   function handleOnClickIncreaseSpeed() {
-    setRotationSpeed(rotationSpeed + 15);
+    setRotationSpeed(rotationSpeed + 15)
   }
 
   // Event invoked when the user clicks the button, the speed will be decreased.
   function handleOnClickDecreaseSpeed() {
-    setRotationSpeed(rotationSpeed - 15);
+    setRotationSpeed(rotationSpeed - 15)
   }
 
   // Event invoked when the user clicks the button, the unity container will be
   // mounted or unmounted depending on the current mounting state.
   function handleOnClickUnMountUnity() {
     if (isLoaded === true) {
-      setIsLoaded(false);
+      setIsLoaded(false)
     }
-    setIsUnityMounted(isUnityMounted === false);
+    setIsUnityMounted(isUnityMounted === false)
   }
 
   // This is the React component that will be rendering the Unity app.
@@ -156,7 +156,7 @@ function App() {
         </h6>
       </div>
     </Fragment>
-  );
+  )
 }
 
-export default App;
+export default App
